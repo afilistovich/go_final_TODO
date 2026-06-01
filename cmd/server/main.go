0c8c434438ee8e7aaa8504ea35be10e089cf1594
Anton Filistovich <anton.filistovich@gmail.com>
2026-06-01 14:40:07 +0300

    feat: implement database initialization

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,16 +1,32 @@
 package main
 
 import (
+	"log"
 	"net/http"
 	"os"
+
+	"github.com/afilistovich/go_final_TODO/internal/db"
 )
 
 const (
-	envPort = "TODO_PORT"
-	webDir  = "./web"
+	envPort   = "TODO_PORT"
+	webDir    = "./web"
+	envDBFile = "TODO_DBFILE"
 )
 
 func main() {
+
+	dbPath := os.Getenv(envDBFile)
+	if dbPath == "" {
+		dbPath = "scheduler.db"
+	}
+
+	err := db.Init(dbPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer db.Close()
 
 	port := os.Getenv(envPort)
 	if port == "" {
@@ -18,5 +34,8 @@ func main() {
 	}
 
 	http.Handle("/", http.FileServer(http.Dir(webDir)))
-	http.ListenAndServe(":"+port, nil)
+	err = http.ListenAndServe(":"+port, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
