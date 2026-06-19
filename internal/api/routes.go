40073a6dd9e2400204279648fdbad696af21072c
Anton Filistovich <anton.filistovich@gmail.com>
2026-06-19 23:21:27 +0300

    add authentication

--- internal/api/routes.go
+++ internal/api/routes.go
@@ -6,13 +6,17 @@ import (
 
 func RegisterRoutes(r chi.Router) {
 
+	r.Post("/api/signin", signInHandler)
 	r.Get("/api/nextdate", nextDateHandler)
-	r.Get("/api/tasks", getTasksHandler)
 
-	r.Get("/api/task", getTaskHandler)
-	r.Post("/api/task", createTaskHandler)
-	r.Put("/api/task", updateTaskHandler)
-	r.Delete("/api/task", deleteTaskHandler)
-	r.Post("/api/task/done", doneTaskHandler)
+	r.Group(func(r chi.Router) {
+		r.Use(auth)
+		r.Get("/api/tasks", getTasksHandler)
 
+		r.Get("/api/task", getTaskHandler)
+		r.Post("/api/task", createTaskHandler)
+		r.Put("/api/task", updateTaskHandler)
+		r.Delete("/api/task", deleteTaskHandler)
+		r.Post("/api/task/done", doneTaskHandler)
+	})
 }
--- tests/settings.go
+++ tests/settings.go
@@ -4,4 +4,4 @@ var Port = 7540
 var DBFile = "../scheduler.db"
 var FullNextDate = true
 var Search = true
-var Token = ``
+var Token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJoYXNoIjoiNTk5NDQ3MWFiYjAxMTEyYWZjYzE4MTU5ZjZjYzc0YjRmNTExYjk5ODA2ZGE1OWIzY2FmNWE5YzE3M2NhY2ZjNSIsImV4cCI6MTc4MTkyODk5OH0.H5cWorq5uUOdwUlyW488DEgasWd8d38XYnjYOY9zIz0`
